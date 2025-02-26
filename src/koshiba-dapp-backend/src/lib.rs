d9642fe9bcd6c048f0d88e655cd4094f501ec384
Taiki <h.tik9603052@gmail.com>
2025-02-26 22:11:59 +0900

    外部APIで遊んでみた

--- src/koshiba-dapp-backend/src/lib.rs
+++ src/koshiba-dapp-backend/src/lib.rs
@@ -1,70 +1,104 @@
-use std::{borrow::Cow, cell::RefCell};
-use candid::{CandidType, Decode, Deserialize, Encode, Principal};
-use ic_stable_structures::memory_manager::{MemoryId, MemoryManager, VirtualMemory};
-use ic_stable_structures::{storable::Bound, DefaultMemoryImpl, StableBTreeMap, Storable};
+use ic_cdk::api::management_canister::http_request::{
+    http_request, CanisterHttpRequestArgument, HttpHeader, HttpMethod, HttpResponse, TransformArgs,
+    TransformContext, TransformFunc
+};
+use serde::{Serialize, Deserialize};
 
-type Memory = VirtualMemory<DefaultMemoryImpl>;
-
-#[derive(CandidType, Deserialize, Debug)]
-struct User {
-  name: String,
+#[derive(Serialize, Deserialize)]
+struct ResponseData {
+    message: String,
+    timestamp: String,
 }
 
-const MAX_VALUE_SIZE: u32 = 100;
+#[ic_cdk::update]
+async fn greet(name: String) -> String {
+    let host = "jsontest.deno.dev";
+    let url = format!("https://{host}/?name={name}");
 
-impl Storable for User {
-  fn to_bytes(&self) -> std::borrow::Cow<[u8]> {
-      Cow::Owned(Encode!(self).unwrap())
-  }
+    let request_headers = vec![
+        HttpHeader {
+            name: "Host".to_string(),
+            value: format!("{host}:443"),
+        },
+        HttpHeader {
+            name: "User-Agent".to_string(),
+            value: "test".to_string(),
+        },
+    ];
 
-  fn from_bytes(bytes: std::borrow::Cow<[u8]>) -> Self {
-      Decode!(bytes.as_ref(), Self).unwrap()
-  }
+    let request = CanisterHttpRequestArgument {
+        url: url.to_string(),
+        method: HttpMethod::GET,
+        body: None,
+        max_response_bytes: None,
+        transform: Some(TransformContext {
+            function: TransformFunc(candid::Func {
+                principal: ic_cdk::api::id(),
+                method: "transform".to_string(),
+            }),
+            context: vec![],
+        }),
+        headers: request_headers,
+    };
 
-  const BOUND: Bound = Bound::Bounded {
-      max_size: MAX_VALUE_SIZE,
-      is_fixed_size: false,
-  };
-}
-
-thread_local! {
-    static MEMORY_MANAGER: RefCell<MemoryManager<DefaultMemoryImpl>> =
-        RefCell::new(MemoryManager::init(DefaultMemoryImpl::default()));
-
-    static MAP: RefCell<StableBTreeMap<Principal, User, Memory>> = RefCell::new(
-        StableBTreeMap::init(
-            MEMORY_MANAGER.with(|m| m.borrow().get(MemoryId::new(0))),
-        )
-    );
-}
-
-#[ic_cdk::query(name="getName")]
-fn get_name() -> Option<String> {
-  let principal = ic_cdk::caller();
-
-  MAP.with(|map|{
-    match map.borrow().get(&principal) {
-        Some(user) => Some(user.name.clone()),
-        None => None
+    let cycles = 230_949_972_000;
+    match http_request(request, cycles).await {
+        Ok((response,)) => {
+            if response.status == 200u64 {
+                String::from_utf8(response.body).unwrap()
+            } else {
+                format!("HTTPS Error. Status:{}", response.status)
+            }
+        },
+        Err((r, m)) => {
+            format!("The http_request resulted into error. RejectionCode: {r:?}, Error: {m}")
+        }
     }
-  })
 }
 
-#[ic_cdk::update(name="setName")]
-fn set_name(name: String) -> () {
-  let principal = ic_cdk::caller();
-  MAP.with(|map|{
-    let mut map = map.borrow_mut();
-    match map.get(&principal) {
-      Some(mut user) => {
-        user.name = name;
-        map.insert(principal, user); // 明示的にinsert()を呼ばないと反映されない
-      },
-      None => {
-        map.insert(principal, User{name});
-      }
+#[ic_cdk::query]
+fn transform(raw: TransformArgs) -> HttpResponse {
+    let headers = vec![
+        HttpHeader {
+            name: "Content-Security-Policy".to_string(),
+            value: "default-src 'self'".to_string(),
+        },
+        HttpHeader {
+            name: "Referrer-Policy".to_string(),
+            value: "strict-origin".to_string(),
+        },
+        HttpHeader {
+            name: "Permissions-Policy".to_string(),
+            value: "geolocation=(self)".to_string(),
+        },
+        HttpHeader {
+            name: "Strict-Transport-Security".to_string(),
+            value: "max-age=63072000".to_string(),
+        },
+        HttpHeader {
+            name: "X-Frame-Options".to_string(),
+            value: "DENY".to_string(),
+        },
+        HttpHeader {
+            name: "X-Content-Type-Options".to_string(),
+            value: "nosniff".to_string(),
+        },
+    ];
+
+    let mut res = HttpResponse {
+        status: raw.response.status.clone(),
+        body: raw.response.body.clone(),
+        headers,
+        ..Default::default()
+    };
+
+    if res.status == 200u64 {
+        let data:ResponseData = serde_json::from_slice(&raw.response.body).unwrap();
+        res.body = data.message.into_bytes()
+    } else {
+        ic_cdk::api::print(format!("Received an error from coinbase: err = {:?}", raw));
     }
-  })
+    res
 }
 
 ic_cdk_macros::export_candid!();
